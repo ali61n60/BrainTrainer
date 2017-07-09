8286ccbe1c14c78edaa5866907d6ca122b8eee59
Ali <ali62n62@yahoo.com>
2017-07-09 19:02:29 +0430

    Layout completed

--- BrainTrainer/MainActivity.cs
+++ BrainTrainer/MainActivity.cs
@@ -1,18 +1,31 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using Android.Views;
 
 namespace BrainTrainer
 {
     [Activity(Label = "BrainTrainer", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
+        private Button _buttonStart;
+
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
 
             // Set our view from the "main" layout resource
             SetContentView (Resource.Layout.Main);
+            initFields();
+        }
+
+        private void initFields()
+        {
+            _buttonStart = FindViewById<Button>(Resource.Id.buttonStart);
+            _buttonStart.Click += (sender, args) =>
+            {
+                
+            };
         }
     }
 }
--- BrainTrainer/Resources/Resource.Designer.cs
+++ BrainTrainer/Resources/Resource.Designer.cs
@@ -60,8 +60,35 @@ namespace BrainTrainer
 		public partial class Id
 		{
 			
+			// aapt resource value: 0x7f050005
+			public const int button0 = 2131034117;
+			
+			// aapt resource value: 0x7f050006
+			public const int button1 = 2131034118;
+			
+			// aapt resource value: 0x7f050007
+			public const int button2 = 2131034119;
+			
+			// aapt resource value: 0x7f050008
+			public const int button3 = 2131034120;
+			
 			// aapt resource value: 0x7f050000
 			public const int buttonStart = 2131034112;
+			
+			// aapt resource value: 0x7f050004
+			public const int gridLayout1 = 2131034116;
+			
+			// aapt resource value: 0x7f050002
+			public const int textViewPoints = 2131034114;
+			
+			// aapt resource value: 0x7f050009
+			public const int textViewResult = 2131034121;
+			
+			// aapt resource value: 0x7f050003
+			public const int textViewSum = 2131034115;
+			
+			// aapt resource value: 0x7f050001
+			public const int textViewTimer = 2131034113;
 			
 			static Id()
 			{
